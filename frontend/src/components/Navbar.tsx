e627cb7a4ff7898c5e20b775512ef10ea1a2d45b
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-04-05 18:13:38 +0530

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
     { name: "About", href: "#about" },
     { name: "Projects", href: "#projects" },
     { name: "Resume", href: "#resume" },
-    { name: "Blog", href: "#blog" },
+    // { name: "Blog", href: "#blog" },
     { name: "Contact", href: "#contact" },
   ];
 
