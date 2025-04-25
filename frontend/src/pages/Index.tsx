0c51a33f3291a91243e36b45df02bf7f04c0bd5f
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-04-25 19:30:22 +0530

    Update Index.tsx

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,8 +1,36 @@
+// import Navbar from "@/components/Navbar";
+// import Hero from "@/components/Hero";
+// import About from "@/components/About";
+// import Projects from "@/components/Projects";
+// //import Blog from '@/components/Blog';
+// import Resume from "@/components/Resume";
+// import Contact from "@/components/Contact";
+// import Footer from "@/components/Footer";
+
+// const Index = () => {
+//   return (
+//     <div className="min-h-screen bg-background text-foreground flex flex-col">
+//       <Navbar />
+//       <div className="flex-1 flex flex-col">
+//         <Hero />
+//         <About />
+//         <Projects />
+//         {/* <Blog /> */}
+//         <Resume/>
+//         <Contact />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Index;
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Projects from "@/components/Projects";
 //import Blog from '@/components/Blog';
+import CodingProfiles from "@/components/CodingProfiles";
 import Resume from "@/components/Resume";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
@@ -15,6 +43,7 @@ const Index = () => {
         <Hero />
         <About />
         <Projects />
+        <CodingProfiles />
         {/* <Blog /> */}
         <Resume/>
         <Contact />
