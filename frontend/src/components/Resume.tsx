1999b48f8fb78f6e6832311724bec6c674c3055f
Abhay Kumar <163289874+BismarckSVBP@users.noreply.github.com>
2025-08-13 17:07:01 +0530

    Update Resume.tsx

--- frontend/src/components/Resume.tsx
+++ frontend/src/components/Resume.tsx
@@ -65,7 +65,7 @@ const Resume = () => {
     const toastId = toast.loading("Downloading resume...");
 
     try {
-      const response = await fetch("/Abhay kumar.pdf");
+      const response = await fetch("/Abhay.pdf");
       if (!response.ok) throw new Error("Failed to fetch resume");
 
       const blob = await response.blob();
